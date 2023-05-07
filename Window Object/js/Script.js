547c386f345576d4f8815f1e3682fc147fc39186
Shivaji singh <shivajisingh54@gmail.com>
2023-05-07 14:04:46 +0530

    first commit

---
+++ Window Object/js/Script.js
@@ -0,0 +1,2 @@
+//alert("Hello shivaji how are you ?");
+// window.alert("Hello shivaji how are you ?"); // same aise bhi likh skte h axcully window bydefault le leta h is liye window likho ya na likho dono sahi hain
