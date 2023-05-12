ef9b5d5513c622023ab454491822e4d9a4a7a669
Shivaji Singh <shivajisingh54@gmail.com>
2023-05-12 15:37:01 +0530

    first commit

---
+++ Complete-JavaScript/JavaScript-Course/Dialog Boxes in js/script.js
@@ -0,0 +1,13 @@
+// alert 
+// const alertResult = alert ("Your message goes here.")
+// console.log(alertResult);
+
+// confirm
+
+// const isConfirmed = confirm ("Your message goes here.")
+// console.log(isConfirmed);
+
+// prompt
+
+ const userInput = prompt ("Your message goes here.")
+ console.log(userInput);
---
+++ Complete-JavaScript/JavaScript-Course/String methods and properties in js/script.js
@@ -0,0 +1,35 @@
+//const message = 'hello, i am Shiavji Singh'
+// console.log(message);
+
+// Property
+//console.log(message.length);
+
+// Methods(Without Argument)
+//console.log(message.toUpperCase());
+//console.log(message.toLowerCase());
+
+//const message1 = '   hello, i am Shiavji Singh   '
+//console.log(message1.trim()); // iska ka hota faltu ka sara space hata deta hain.
+//console.log(message1.trim().toUpperCase()); // trim method ke sth toUpperCase and toLowerCase method ko likh skte hain.
+//console.log(message1.trimStart()); // aur iska km h aage se faltu space hata dena.
+//console.log(message1.trimEnd()); // aur iska km h pichhe se faltu ka space hata dena.
+
+//Method (With Argument)
+//const mess1 ='hiii i am shiva';
+//const mess = "hello everyone how are you";
+//console.log(mess.includes('how')); // ye ek bada string choti string ko check krta h agar hoga to true and nhi hoga to false return krta hain.
+//console.log(mess.indexOf('o')); // ye hame indexing deta hain wo starting se jo pehle aayega usi ka indexing dega aur nhi milega to -1 return kr dega.
+//console.log(mess.replace('a','shiva')); // iska km hota h jo value daloge us jagah dusari value daloge to pehle wali replace kr degi new wali se.
+//console.log(mess.replaceAll('r','golu')); // iska km saare value ko replace kr dega new value se.
+//console.log(mess.concat(mess1 + " , " + mess)); // iska km hota h add krna do ya do se aadhik string ko.
+
+// const digits = '7856'
+// console.log(digits.padStart(16,'*'));
+// console.log(digits.padEnd(16,'*'));
+
+// const charactors = 'hello my name is shiva';
+// console.log(charactors.charAt('7')); // ye charactor ki indexing batata hain.
+
+const message = 'hello everyone i Am shiva';
+//console.log(message.charCodeAt(17)); // ye hume acii code deta hain jo index hum put krte hain uska.
+console.log(message.split('')); // ye value ko split krta hain.
