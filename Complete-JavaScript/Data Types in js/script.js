ae606f2904944b2c786fce25466b6ad154998d68
Shivaji Singh <106834733+RajputShiva@users.noreply.github.com>
2023-06-27 18:16:41 +0530

    Delete Complete-JavaScript directory

--- Complete-JavaScript/Data Types in js/script.js
+++
@@ -1,2 +0,0 @@
-console.log('hello world!!!');
-console.log(7+9);// short me clg likhne pr console.log likha jata hain
--- Complete-JavaScript/code-execution-in-js/script.js
+++
@@ -1,8 +0,0 @@
-var firstName = 'Shivaji'
-let lastName = 'Singh'
-let age = 27
-const yearOfBirth = 1996
-
-let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
-
-console.log(userIntro);
